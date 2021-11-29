aeae93d6cf189ac4e7980714c11d1fd94477641d
Eric L. Hepperle <codewizard13@gmail.com>
2021-11-29 11:41:17 -0600

    Add js/ folder. Create demo.js

---
+++ js/demo.js
@@ -0,0 +1,32 @@
+/*
+Program Name:   EHW JavaScript ES6 Demo: Add element to DOM (prepend)
+File Name:      demo.js
+Date Created:   11/29/21
+Date Modified:  --
+Version:        1.0
+Programmer:     Eric L. Hepperle
+
+TAGS:           Eric Hepperle, JavaScript, ES6, DOM Manipulation, Demo
+
+Purpose: 
+    HTML/PHP based sandbox to test web code. This is a demo for testing
+    JavaScript. The project folder must live on a web server, local or remote.
+    
+Usage:
+    Use with controller.php
+
+Sample results: 
+
+    Tutorial Info:
+    - Title: How to Create Photo Gallery Grid with Modal Window Lightbox
+    - URL: https://www.youtube.com/watch?v=IPVQeVlw2_E
+    - Channel: LearnWebCode
+
+Requires:
+	* Browser opened to a YouTube video.
+    
+Demonstrates:
+    * Vanilla JavaScript
+    * JavaScript ES6/ECMAScript2015
+
+*/
