83a005c4ac6679033674538b4bbe7deeb39b5931
Eric L. Hepperle <codewizard13@gmail.com>
2021-11-29 10:58:58 -0600

    Add JSON HTML output formatting func (not working)

--- controller.php
+++ controller.php
@@ -250,10 +250,20 @@ function appendData(data) {
 }
 */
 
+// not working with foreach yet ...
+function formatJSONAutos_01(obj) {
+    
+    let keys = Object.keys(obj);
+    console.log(`keys: ${keys}`);
+    
+    for (let i=0; i < keys.length; i++) {
+        let p = document.createElement('p');
+        console.log(obj[i]);
+    }
+}
 
 
-// appendData(content);
-
+formatJSONAutos_01(data);
 
 
 
